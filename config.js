bedf8bc863f09890440279bc21a827c2c9b60b28
Ian McDermott <imcd.po@gmail.com>
2017-12-22 00:19:33 -0500

    Removed problematic parentheses

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ exports.DATABASE_URL =
 	process.env.DATABASE_URL ||
 	global.DATABASE_URL ||
 	'mongodb://localhost/desktop/p5js/artapp';
-	exports.PORT = process.env.PORT || 8080;
-	exports.JWT_SECRET = process.env.JWT_SECRET || '!Hop3Nobody5eesThis';
-	exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET || '!Hop3Nobody5eesThis';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 
-exports.TEST_DATABASE_URL = (
+exports.TEST_DATABASE_URL = 
 	process.env.TEST_DATABASE_URL ||
-	'mongodb://localhost/desktop/p5js/artapp');
+	'mongodb://localhost/desktop/p5js/artapp';
